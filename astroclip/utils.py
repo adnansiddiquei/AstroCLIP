d55af2969ce067e41d38c07f5cf34c48b7ab6a8f
adnansiddiquei <adnanshirik@gmail.com>
2024-05-16 17:29:49 +0100

    Added data download script for the DESI Legacy Survey Data Release 9 which was used in the original paper and prepared by Stein et al. (2021).

--- astroclip/utils.py
+++ astroclip/utils.py
@@ -2,6 +2,8 @@ import torch.nn as nn
 import torch
 import numpy as np
 import pytorch_lightning as L
+import os
+from datasets import load_dataset
 
 
 def copy_weights(source: nn.Module, target: nn.Module) -> None:
@@ -25,3 +27,44 @@ def calc_model_parameter_count(model: nn.Module | L.LightningModule) -> int:
     Calculate the number of parameters in a model
     """
     return np.sum([p.numel() for p in model.parameters()])
+
+
+def create_dir_if_required(script_filepath: str, dir_name: str) -> str:
+    """
+    Create a directory if it does not exist.
+
+    Parameters
+    ----------
+    script_filepath : str
+        The path to the script calling this function.
+    dir_name : str
+        The name of the directory to create, relative to the path provided in the first function.
+
+    Returns
+    -------
+    str
+        The path to the directory created.
+    """
+    cwd = os.path.dirname(os.path.realpath(script_filepath))
+    dir_to_make = os.path.join(cwd, dir_name)
+
+    if not os.path.exists(os.path.join(cwd, dir_name)):
+        os.makedirs(dir_to_make)
+
+    return dir_to_make
+
+
+def download_desi_dataset():
+    cwd = os.path.dirname(os.path.realpath(__file__))
+
+    cache_dir = create_dir_if_required(__file__, '../cache')
+    dataset_dir = f'{cache_dir}/desi_dataset'
+
+    dataset = load_dataset(
+        f'{cwd}/../scripts/legacy_survey.py',
+        cache_dir=dataset_dir,
+        trust_remote_code=True,
+    )
+    dataset.set_format(type='torch')
+
+    return dataset
---
+++ scripts/download_desi_dataset.py
@@ -0,0 +1,5 @@
+from astroclip.utils import download_desi_dataset
+
+
+if __name__ == '__main__':
+    download_desi_dataset()
