e8f752966c77ded2fb94a8df81fdc8db9faa747b
adnansiddiquei <adnanshirik@gmail.com>
2024-05-21 23:13:02 +0100

    Amended ContrastiveBimodalPretraining class to take a pre_transforms and a post_transforms to fix issue with requiring some transforms to be applied before augmentations, and some needing to be applied after.

--- astroclip/models.py
+++ astroclip/models.py
@@ -14,8 +14,9 @@ class ContrastiveBimodalPretraining(L.LightningModule):
     def __init__(
         self,
         encoders: list[nn.Module],
+        pre_transforms: list[nn.Sequential | None] = (None, None),
         augmentations: list[nn.Sequential | None] = (None, None),
-        transforms: list[nn.Sequential | None] = (None, None),
+        post_transforms: list[nn.Sequential | None] = (None, None),
         loss: nn.Module = InfoNCELoss(),
         learning_rate=5e-4,
         modality_names: list[str] = ('modality1', 'modality2'),
@@ -33,14 +34,15 @@ class ContrastiveBimodalPretraining(L.LightningModule):
             module (the encoders) which takes in an input and produces an embedding. The embeddings produced by the
             encoders must be of the same dimension. If the models are pretrained and only require fine-tuning, the
             relevant weights should be frozen before passing them to this module.
+        pre_transforms : list[nn.Sequential | None]
+            List of two pre_transforms, one for each modality. This must be a list of length 2, each element being a
+            torch.nn.Sequential object which takes in an input and applies a series of transformations to it.
+            If no post_transforms are required for a modality, the corresponding element in the list should be None.
+            These transformations are applied before the augmentations.
         augmentations : list[nn.Sequential | None]
-            List of two augmentations, one for each modality. This must be a list of length 2, each element being a
-            torch.nn.Sequential object which takes in an input and applies a series of transformations to it.
-            If no augmentations are required for a modality, the corresponding element in the list should be None.
-        transforms : list[nn.Sequential | None]
-            List of two transforms, one for each modality. This must be a list of length 2, each element being a
-            torch.nn.Sequential object which takes in an input and applies a series of transformations to it.
-            If no transforms are required for a modality, the corresponding element in the list should be None.
+            List of two augmentations, one for each modality. Formatting is the same as for pre_transforms.
+        post_transforms : list[nn.Sequential | None]
+            List of two post_transforms, one for each modality. Formatting is the same as for pre_transforms.
         loss : nn.Module
             The loss function to use for training. This should be a PyTorch module which takes in two tensors of shape
             (batch_size, embedding_dim) and returns a scalar loss.
@@ -49,7 +51,7 @@ class ContrastiveBimodalPretraining(L.LightningModule):
         modality_names : list[str]
             List of two strings, the names of the modalities. These names will be used to access the modalities in the
             batch dictionary. The default is ['modality1', 'modality2']. The order of the names should match the order
-            of the encoders, augmentations, and transforms. In the case of AstroCLIP, these names would be ['image',
+            of the encoders, augmentations, and post_transforms. In the case of AstroCLIP, these names would be ['image',
             'spectrum'].
         """
         super().__init__()
@@ -59,16 +61,21 @@ class ContrastiveBimodalPretraining(L.LightningModule):
         ), 'This module requires exactly two encoders, one for each modality'
 
         assert (
-            len(transforms) == 2
-        ), 'This module requires exactly two transforms, one for each modality'
+            len(pre_transforms) == 2
+        ), 'This module requires exactly two pre_transforms, one for each modality'
+
+        assert (
+            len(post_transforms) == 2
+        ), 'This module requires exactly two post_transforms, one for each modality'
 
         assert (
             len(augmentations) == 2
         ), 'This module requires exactly two augmentations, one for each modality'
 
         self.mod1_encoder, self.mod2_encoder = encoders
+        self.mod1_pre_transforms, self.mod2_pre_transforms = pre_transforms
         self.mod1_augmentations, self.mod2_augmentations = augmentations
-        self.mod1_transforms, self.mod2_transforms = transforms
+        self.mod1_post_transforms, self.mod2_transforms = post_transforms
         self.mod1_name, self.mod2_name = modality_names
 
         self.loss_func = loss
@@ -116,12 +123,33 @@ class ContrastiveBimodalPretraining(L.LightningModule):
 
         return {self.mod1_name: mod1, self.mod2_name: mod2}
 
-    def _apply_transforms(self, batch) -> dict:
+    def _apply_post_transforms(self, batch) -> dict:
         mod1, mod2 = batch[self.mod1_name], batch[self.mod2_name]
 
-        mod1 = self.mod1_transforms(mod1) if self.mod1_transforms is not None else mod1
+        mod1 = (
+            self.mod1_post_transforms(mod1)
+            if self.mod1_post_transforms is not None
+            else mod1
+        )
 
         mod2 = self.mod2_transforms(mod2) if self.mod2_transforms is not None else mod2
+
+        return {self.mod1_name: mod1, self.mod2_name: mod2}
+
+    def _apply_pre_transforms(self, batch) -> dict:
+        mod1, mod2 = batch[self.mod1_name], batch[self.mod2_name]
+
+        mod1 = (
+            self.mod1_pre_transforms(mod1)
+            if self.mod1_pre_transforms is not None
+            else mod1
+        )
+
+        mod2 = (
+            self.mod2_pre_transforms(mod2)
+            if self.mod2_pre_transforms is not None
+            else mod2
+        )
 
         return {self.mod1_name: mod1, self.mod2_name: mod2}
 
@@ -140,11 +168,12 @@ class ContrastiveBimodalPretraining(L.LightningModule):
             self.mod2_name in batch.keys()
         ), f"Batch must contain key '{self.mod1_name}'"
 
-        # Apply augmentations and transforms to the modalities
+        batch = self._apply_pre_transforms(batch)
+
+        # Apply augmentations only if training
         if self.trainer.training:
-            # perform the augmentations only if training, we do not want to augment the data during validation
             batch = self._apply_augmentations(batch)
 
-        batch = self._apply_transforms(batch)
+        batch = self._apply_post_transforms(batch)
 
         return batch
